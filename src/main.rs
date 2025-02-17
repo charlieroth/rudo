93311170eb32dfc008fd0159487af225c17e3c6d
charlieroth <charlieroth4@gmail.com>
2025-02-17 19:09:14 +0100

    setup simple clap application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[command(name = "add", about = "Add a new task")]
+    Add { title: String },
+    #[command(name = "list", about = "List all tasks")]
+    List,
+    #[command(name = "delete", about = "Delete a task")]
+    Delete { id: String },
+    #[command(name = "do", about = "Mark a task as done")]
+    Do { id: String },
+    #[command(name = "undo", about = "Mark a task as undone")]
+    Undo { id: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Add { title } => {
+            println!("Adding task: {}", title);
+        }
+        Commands::List => {
+            println!("Listing all tasks");
+        }
+        Commands::Do { id } => {
+            println!("Marking task as done: {}", id);
+        }
+        Commands::Undo { id } => {
+            println!("Marking task as undone: {}", id);
+        }
+        Commands::Delete { id } => {
+            println!("Deleting task: {}", id);
+        }
+    }
 }
